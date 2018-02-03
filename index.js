ac1eab338e3a6ff20d3eed5c82a7e5337440fb49
talkot <tkot@vidazoo.com>
2018-02-03 21:07:31 +0200

    new heroku

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res)=>{
-  res.send({'test' : 'testing!'});
-});
-
-const PORT = process.env.port || 5000;
+app.get('/', (req, res) => {
+    res.send({'test': 'testing!'});
+})
+;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
 console.log('app listen on port 5000');
