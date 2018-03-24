4c7b3ee039bffe8a881aedd210d3972bd22510e3
talkot <tkot@vidazoo.com>
2018-03-24 13:36:00 +0300

    version 1.4.1

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -4,7 +4,7 @@ const tslib_1 = require("tslib");
 const commentController_1 = require("../controllers/commentController");
 const requireLogin = require('../middlewares/requireLogin');
 module.exports = app => {
-    app.post('/api/comment/add', (req, res) => tslib_1.__awaiter(this, void 0, void 0, function* () {
+    app.post('/api/comment/add', requireLogin, (req, res) => tslib_1.__awaiter(this, void 0, void 0, function* () {
         let { rating, seller, comment } = req.body;
         let commentControllerInstance = commentController_1.default.Instance;
         yield commentControllerInstance.postComment(res, rating, seller, comment, req.user.id);
--- routes/commentRoutes.ts
+++ routes/commentRoutes.ts
@@ -5,7 +5,7 @@ const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
 
-    app.post('/api/comment/add', async (req, res) => {
+    app.post('/api/comment/add', requireLogin, async (req, res) => {
         let {rating, seller, comment} = req.body;
         let commentControllerInstance = CommentController.Instance;
         await commentControllerInstance.postComment(res, rating, seller, comment, req.user.id);
